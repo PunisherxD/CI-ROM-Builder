98789ff259b5197c2a78f46ecf6ef1086a9f4986
PunisherxD <134217435+PunisherxD@users.noreply.github.com>
2023-05-21 20:44:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BiancaProject/android.git -b 13-meme -g default,-mips,-darwin,-notdefault
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync --current-branch -j16
-git clone --depth=1 https://${PAT_KEY}@github.com/chaldeaprjkt/device_xiaomi_vayu -b 13-dudu device/xiaomi/vayu
-git clone --depth=1 https://${PAT_KEY}@github.com/chaldeaprjkt/vendor_xiaomi_vayu -b 13-dudu vendor/xiaomi/vayu
-git clone --depth=1 https://github.com/aLnProject/device_xiaomi_vayu-kernel -b main device/xiaomi/vayu-kernel
-git clone --depth=1 https://${LAB_KEY}@gitlab.com/BiancaProject/vendor_xiaomi_miuicamera -b leica vendor/xiaomi/miuicamera
+repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
+git clone --depth=1 https://github.com/PunisherxD/device_motorola_liber -b VoltageOS device/motorola/liber
+git clone --depth=1 https://github.com/menorziin/vendor_motorola_liber -b lineage-20 vendor/motorola/liber
+git clone --depth=1 https://github.com/menorziin/kernel_motorola_liber -b lineage-20 kernel/motorola/liber
+git clone --depth=1 https://github.com/sm6150-liber/android_packages_apps_MotoActions -b lineage-20.0 packages/apps/MotoActions
 
 # build rom
+curl -Lo barom.sh https://raw.githubusercontent.com/alanndz/barom/main/barom.sh
+chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
-./barom.sh -s ${SF_USER} ${SF_PASS} ${SF_USER}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
-./barom.sh --device vayu --lunch vayu-user
-./barom.sh -b -j 8 -u "gof sf" --timer 95m -- m dudu
+./barom.sh --device liber --lunch liber-userdebug
+./barom.sh -b -j 8 -u gof --timer 95m -- m dudu
 
 # end
