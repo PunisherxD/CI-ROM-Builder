acbf2a1f5a575cdb03265dc7795e1f62b2f83e97
PunisherxD <134217435+PunisherxD@users.noreply.github.com>
2023-05-21 20:43:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BiancaProject/android.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-git clone --depth=1 https://${PAT_KEY}@github.com/chaldeaprjkt/device_xiaomi_whyred -b t44 device/xiaomi/whyred
-git clone --depth=1 https://${PAT_KEY}@github.com/chaldeaprjkt/vendor_xiaomi_whyred -b t44 vendor/xiaomi
-git clone --depth=1 https://github.com/Mocaness/device_xiaomi_whyred-kernel -b t44 device/xiaomi/whyred-kernel
-git clone --depth=1 https://${PAT_KEY}@github.com/Mocaness/keys-dev -b master certs
-git clone --depth=1 https://${PAT_KEY}@github.com/Mocaness/vendor_scrapyard -b 13 vendor/scrapyard
-git clone --depth=1 https://github.com/LineageOS/android_packages_apps_Aperture -b lineage-20.0 packages/apps/Aperture
-git clone --depth=1 https://github.com/Mocarafee/device_bloatware -b nad_s device/bloatware
+git clone --depth=1 https://github.com/PunisherxD/device_motorola_liber -b VoltageOS device/motorola/liber
+git clone --depth=1 https://github.com/menorziin/vendor_motorola_liber -b lineage-20 vendor/motorola/liber
+git clone --depth=1 https://github.com/menorziin/kernel_motorola_liber -b lineage-20 kernel/motorola/liber
+git clone --depth=1 https://github.com/sm6150-liber/android_packages_apps_MotoActions -b lineage-20.0 packages/apps/MotoActions
 
 # build rom
 curl -Lo barom.sh https://raw.githubusercontent.com/alanndz/barom/main/barom.sh
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
-./barom.sh --device whyred --lunch whyred-user
+./barom.sh --device liber --lunch liber-userdebug
 ./barom.sh -b -j 8 -u gof --timer 95m -- m dudu
 
 # end
