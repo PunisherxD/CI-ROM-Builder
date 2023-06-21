0c5fb5fa89eeb96ba12eaa7db480b133088d310d
PunisherxD <134217435+PunisherxD@users.noreply.github.com>
2023-06-21 13:24:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #  sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BananaDroid/android_manifest.git -b 13 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/DESTROYER3264/local_manifest --depth 1 -b banana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
 
@@ -8,7 +8,7 @@ curl -Lo barom.sh https://raw.githubusercontent.com/alanndz/barom/main/barom.sh
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
-./barom.sh --device liber --lunch banana_liber-userdebug
-./barom.sh -b -j 8 -u "gof sf fio" --timer 95m -- m banana
+./barom.sh --device liber --lunch pixys_liber-userdebug
+./barom.sh -b -j 8 -n PixysOS -u "gof sf fio" --timer 95m -- make pixys
 
 # end
