2f3dee47bcb2f75e951494a9371b0260f3cb9907
PunisherxD <134217435+PunisherxD@users.noreply.github.com>
2023-06-30 16:26:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/DESTROYER3264/local_manifest --depth 1 -b banana .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/DESTROYER3264/local_manifest --depth 1 -b cr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
 
 # build rom
@@ -8,7 +8,7 @@ curl -Lo barom.sh https://raw.githubusercontent.com/alanndz/barom/main/barom.sh
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
-./barom.sh --device liber --lunch pixys_liber-userdebug
-./barom.sh -b -j 8 -n PixysOS -u "gof sf fio" --timer 95m -- make pixys
+./barom.sh --device liber --lunch lineage_liber-userdebug
+./barom.sh -b -j 8 -n crDroidAndroid -u "gof sf fio" --timer 95m -- m bacon
 
 # end
