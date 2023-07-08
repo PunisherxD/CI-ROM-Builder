78489b4877f545eedd10326b13043ae27444d401
PunisherxD <134217435+PunisherxD@users.noreply.github.com>
2023-07-08 15:09:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #  sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/DESTROYER3264/local_manifest --depth 1 -b cr .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/DESTROYER3264/local_manifest --depth 1 -b bliss .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
 
 # build rom
@@ -8,7 +8,7 @@ curl -Lo barom.sh https://raw.githubusercontent.com/alanndz/barom/main/barom.sh
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
-./barom.sh --device liber --lunch blaze_liber-userdebug
-./barom.sh -b -j 8 -n ProjectBlaze -u "gof sf fio" --timer 95m -- brunch liber
+./barom.sh --device liber --lunch bliss_liber-userdebug
+./barom.sh -b -j 8 -n Bliss -u "gof sf fio" --timer 95m -- blissify -g liber
 
 # end
