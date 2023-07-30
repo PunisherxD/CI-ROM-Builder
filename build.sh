439d0dab3be9a4961677f49a2a30124cc253eb1d
PunisherxD <134217435+PunisherxD@users.noreply.github.com>
2023-07-30 13:18:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #  sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/DESTROYER3264/local_manifest --depth 1 -b vos .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/DESTROYER-32/local_manifest --depth 1 -b rising .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
 
 # build rom
@@ -8,7 +8,7 @@ curl -Lo barom.sh https://raw.githubusercontent.com/alanndz/barom/main/barom.sh
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
-./barom.sh --device liber --lunch voltage_liber-userdebug
-./barom.sh -b -j 6 -n voltage -u "gof sf fio" --timer 95m -- brunch voltage_liber-userdebug
+./barom.sh --device liber --lunch lineage_liber-userdebug
+./barom.sh -b -j 8 -n risingOS -u "gof sf fio" --timer 95m -- brunch lineage_liber-userdebug
 
 # end
