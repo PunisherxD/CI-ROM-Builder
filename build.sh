4c8d40af4bf2860f4bd690b23ca69119ddf809d9
PunisherxD <134217435+PunisherxD@users.noreply.github.com>
2023-10-24 17:58:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #  sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/DESTROYER-32/local_manifest --depth 1 -b rising .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
+#git clone https://github.com/DESTROYER-32/local_manifest --depth 1 -b rising .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
 
 # build rom
@@ -8,7 +8,7 @@ curl -Lo barom.sh https://raw.githubusercontent.com/alanndz/barom/main/barom.sh
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
-./barom.sh --device liber --lunch lineage_liber-userdebug
-./barom.sh -b -j4 -j 4 -n risingOS -u "gof sf fio" --timer 95m -- brunch lineage_liber-userdebug
+./barom.sh --device dubai --lunch lineage_dubai-userdebug
+./barom.sh -b -j4 -j 4 -n lineageOS -u "gof sf fio" --timer 95m -- brunch lineage_dubai-userdebug
 
 # end
